eb20c27b5d2f195468ffc7c518e29cc407a3c1fc
Andee13 <39676479+Andee13@users.noreply.github.com>
2018-10-28 21:49:11 +0200

    Initial commit

---
+++ src/main/java/mvn_test/App.java
@@ -0,0 +1,16 @@
+package mvn_test;
+
+
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
+public class App 
+{
+    private static final Logger logger = org.apache.logging.log4j.LogManager.getLogger(App.class.getName());
+    public static void main( String[] args )
+    {
+
+        System.out.println( "Hello World!" );
+
+
+    }
+}
