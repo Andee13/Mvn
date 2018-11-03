70b7cb2bd5bbb239c3005aa4be3e48ecc7b3d964
Andee13 <39676479+Andee13@users.noreply.github.com>
2018-11-03 19:46:41 +0200

    commit1

--- src/main/java/mvn_test/Main.java
+++ src/main/java/mvn_test/Main.java
@@ -4,7 +4,7 @@ import org.apache.logging.log4j.*;
 //import org.apache.logging.log4j.LogManager;
 
 public class Main{
-    static  final Logger logger = LogManager.getLogger(Main.class);
+    private static  final Logger logger = LogManager.getLogger(Main.class);
 
     public static void main(String[] args) {
 
